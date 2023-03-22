4fcda6627b1abe8752fd18ba18ca8a8348b918d9
Louis-Vu <hoangvuhuy04@gmail.com>
2023-03-22 08:45:42 +0700

    java

---
+++ src/com/class1/MainClass.java
@@ -0,0 +1,20 @@
+package com.class1;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+            //Create objects
+            Person person1 = new Person();
+            Person person2 = new Person();
+            Person person3 = new Person();
+            Person person4 = new Person();
+
+            person1.inputData();
+            person1.display();
+
+    }
+
+}
